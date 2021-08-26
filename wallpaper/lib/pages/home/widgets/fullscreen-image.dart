be3201d9f068d8a850c877a6b03f70993b630752
ibrahim duman <ibrahim.duman@roofstacks.com>
2021-08-26 14:00:18 +0300

    iborahim

--- wallpaper/lib/pages/home/widgets/fullscreen-image.dart
+++ wallpaper/lib/pages/home/widgets/fullscreen-image.dart
@@ -1,177 +1,45 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:wallpaper/pages/home/home-models.dart';
-import 'package:wallpaper/pages/home/widgets/animation-fab/expandable-fab.dart';
-import 'package:wallpaper/pages/home/widgets/fullscreen-fla.dart';
-import 'package:wallpaper/pages/home/home-setwallpaper.dart';
-import 'package:wallpaper/shared/custom-widgets/custom-slide-trans.dart';
-import 'package:wallpaper/shared/custom-widgets/custom-text.dart';
-import 'package:wallpaper/shared/database/app-cruds.dart';
 
-class FullScreenImage extends StatefulWidget {
+class FullScreenImage extends StatelessWidget {
   Images image;
   FullScreenImage({required this.image});
 
   @override
-  _FullScreenImageState createState() => _FullScreenImageState();
-}
-
-class _FullScreenImageState extends State<FullScreenImage> {
-  AppCruds _appCruds = AppCruds();
-
-  bool isFavorite = false;
-
-  @override
-  void initState() {
-    super.initState();
-    isFav();
-  }
-
-  Future isFav() async {
-    print(widget.image.image);
-    var a = await _appCruds.isFavorite(widget.image);
-    setState(() {
-      isFavorite = a != null;
-    });
-
-    @override
-    Widget build(BuildContext context) {
-      // TODO: implement build
-      throw UnimplementedError();
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        Container(
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage('${image.thumb}'),
+          fit: BoxFit.fill
+        ),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
+        child: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
-                image: NetworkImage('${widget.image.thumb}'), fit: BoxFit.fill),
+              image: NetworkImage('${image.thumb}'),
+              fit: BoxFit.contain
+            ),
           ),
           child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage('${widget.image.thumb}'),
-                    fit: BoxFit.contain),
-              ),
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                child: Image.network(
-                  '${widget.image.orjImg}',
-                  fit: BoxFit.cover,
-                  loadingBuilder: (context, child, event) {
-                    if (event == null) {
-                      return child;
-                    }
-                    return Center(child: CircularProgressIndicator());
-                  },
-                ),
-              ),
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: Image.network(
+              '${image.orjImg}',
+              fit: BoxFit.cover,
+              loadingBuilder: (context, child, event) {
+                if (event == null) {
+                  return child;
+                }
+                return Center(child: CircularProgressIndicator());
+              },
             ),
           ),
-        ),
-        /* Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 40,
-            color: Colors.white.withOpacity(0.3),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextButton.icon(
-                  onPressed: () => favoriteButton(),
-                  icon: Icon(
-                      isFavorite ? Icons.favorite : Icons.favorite_outline,
-                      color: Colors.black),
-                  label: CustomText(
-                    text: '',
-                  ),
-                ),
-                TextButton.icon(
-                  onPressed: () {
-                    showModalBottomSheet(
-                        backgroundColor: Colors.transparent.withOpacity(0.5),
-                        isScrollControlled: true,
-                        context: context,
-                        builder: (context) {
-                          return Container(
-                            child: Column(
-                              children: [
-                                Expanded(
-                                  child: optionMenu('Main Screen', 950),
-                                ),
-                                Expanded(
-                                  child: optionMenu('Lock Screen', 1150),
-                                ),
-                                Expanded(
-                                  child: optionMenu('Both Screen', 1350),
-                                ),
-                              ],
-                            ),
-                          );
-                        });
-                  },
-                  icon: Icon(
-                    Icons.add_to_home_screen,
-                    color: Colors.black,
-                  ),
-                  label: CustomText(
-                    text: '',
-                  ),
-                ),
-                TextButton.icon(
-                  onPressed: moreButton(),
-                  icon: Icon(Icons.more_horiz, color: Colors.black),
-                  label: CustomText(
-                    text: '',
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomLeft,
-          child: FullScreenFla(isFavorite: isFavorite,),
-        ), */
-        Align(
-          alignment: Alignment.bottomRight,
-          child: ExpandableFab(isFavorite: isFavorite, image: widget.image),
-        )
-      ],
-    );
-  }
-
-  optionMenu(String text, int duration) {
-    return GestureDetector(
-      onTap: () => null,
-      child: Container(
-        child: CustomSlideTransition(
-          text: '$text',
-          duration: duration,
         ),
       ),
     );
   }
 
-  favoriteButton() async {
-    print('isfavorite: $isFavorite');
-    if (isFavorite) {
-      await _appCruds.delete(widget.image);
-    } else {
-      await _appCruds.insert(widget.image);
-    }
-    setState(() {
-      isFavorite = !isFavorite;
-    });
-  }
-
-  moreButton() {
-    print("burayada geldin ya");
-  }
 }
---
+++ wallpaper/lib/pages/home/widgets/fullscreen-page.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaper/pages/home/home-models.dart';
+import 'package:wallpaper/pages/home/widgets/fullscreen-image.dart';
+import 'package:wallpaper/shared/custom-widgets/custom-slide-trans.dart';
+import 'package:wallpaper/shared/custom-widgets/custom-text.dart';
+import 'package:wallpaper/shared/database/app-cruds.dart';
+
+class FullscreenPage extends StatefulWidget {
+  Images image;
+  FullscreenPage({required this.image});
+
+  @override
+  _FullscreenPageState createState() => _FullscreenPageState();
+}
+
+class _FullscreenPageState extends State<FullscreenPage> {
+  AppCruds _appCruds = AppCruds();
+  bool isFavorite = false;
+  
+  @override
+  void initState() {
+    super.initState();
+    isFav();
+  }
+
+  Future isFav() async {
+    print(widget.image.image);
+    var a = await _appCruds.isFavorite(widget.image);
+    setState(() {
+      isFavorite = a != null;
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          IconButton(
+            icon: Icon(isFavorite ? Icons.favorite : Icons.favorite_outline, color: Colors.white,),
+            onPressed: () => favoriteButton(),
+          ),
+          IconButton(
+            icon: Icon(Icons.more_horiz, color: Colors.white),
+            onPressed: () => moreMenu(context, widget.image),
+          )
+        ],
+        centerTitle: true,
+      ),
+      body: Center(
+        child: FullScreenImage(image: widget.image,),
+      ),
+      bottomNavigationBar: Container(
+        height: 90,
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              icon: Icon(Icons.insert_photo, color: Colors.white, size: 35,),
+              onPressed: () {
+                showModalBottomSheet(
+                    backgroundColor: Colors.transparent.withOpacity(0.5),
+                    isScrollControlled: true,
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: optionMenu(
+                                  'Main Screen', 650, widget.image.image),
+                            ),
+                            Expanded(
+                              child: optionMenu(
+                                  'Lock Screen', 850, widget.image.image),
+                            ),
+                            Expanded(
+                              child: optionMenu(
+                                  'Both Screen', 1050, widget.image.image),
+                            ),
+                          ],
+                        ),
+                      );
+                    });
+              },
+            ),
+            SizedBox(width: 20),
+            IconButton(
+              icon: Icon(Icons.download, color: Colors.white, size: 35),
+              onPressed: null,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  favoriteButton() async {
+    if (isFavorite) {
+      await _appCruds.delete(widget.image);
+    } else {
+      await _appCruds.insert(widget.image);
+    }
+    setState(() {
+      isFavorite = !isFavorite;
+    });
+  }
+
+  optionMenu(String text, int duration, String ourl) {
+    return Container(
+      child: GestureDetector(
+        child: CustomSlideTransition(
+          text: '$text',
+          duration: duration,
+        ),
+      ),
+    );
+  }
+
+  moreMenu(BuildContext context, Images image){
+    showModalBottomSheet(
+      backgroundColor: Colors.transparent.withOpacity(0.5),
+      isScrollControlled: true,
+      context: context,
+      builder: (context){
+        return Container(
+          height: MediaQuery.of(context).size.height/3,
+          color: Colors.black.withOpacity(0.5),
+          child: Center(
+            child: CustomText(text: image.id, color: Colors.white, fontWeight: FontWeight.bold,),
+          ),
+        );
+      }
+    );
+  }
+}
